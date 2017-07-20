a1313c18a0f607817bdbecc41fa98612a04c6ca9
delen01 <delen01@ca.com>
2017-07-20 11:44:10 +0200

    initial attributes values xsi:type(s) for SAML2 IdP

--- modules/saml/lib/IdP/SAML2.php
+++ modules/saml/lib/IdP/SAML2.php
@@ -1065,6 +1065,48 @@ class SAML2
 
 
     /**
+     * Determine attribute value xsi:type(s), merging configs in the following order:
+     *  IdP
+     *  Sp
+     *  State
+     *
+     * so State config will have the precedence
+     *
+     * @param SimpleSAML_Configuration $idpMetadata The metadata of the IdP.
+     * @param SimpleSAML_Configuration $spMetadata The metadata of the SP.
+     * @param array $state.
+     *
+     * @return array resolved array.
+     */
+    private static function getAttributesValuesTypes(
+        SimpleSAML_Configuration $idpMetadata,
+        SimpleSAML_Configuration $spMetadata,
+        array &$state
+    ) {
+
+        // get config from idp config
+        $idpavt = $idpMetadata->getArray('attributesValuesTypes', array());
+
+        // get config from sp config
+        $spavt = $spMetadata->getArray('attributesValuesTypes', array());
+
+        // get config from state
+        if (array_key_exists('attributesValuesTypes',$state)) {
+            $stateavt = $state['attributesValuesTypes'];
+            if (!is_array($stateavt)) {
+                $stateavt = array();
+            }
+        } else {
+            $stateavt = array();
+        }
+
+        // merge them all
+        $finalavt = array_merge($idpavt,$spavt,$stateavt);
+
+        return $finalavt;
+    }
+
+    /**
      * Build an assertion based on information in the metadata.
      *
      * @param \SimpleSAML\Configuration $idpMetadata The metadata of the IdP.
@@ -1195,6 +1237,8 @@ class SAML2
             $a->setAttributeNameFormat($attributeNameFormat);
             $attributes = self::encodeAttributes($idpMetadata, $spMetadata, $state['Attributes']);
             $a->setAttributes($attributes);
+            $attributesValuesTypes = self::getAttributesValuesTypes($idpMetadata, $spMetadata, $state);
+            $a->setAttributesValueTypes($attributesValuesTypes);
         }
 
         $nameIdFormat = null;
